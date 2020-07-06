27a0b47781d4f1df720ae30c5cb51030daec7035
ShawnMeister <45756873+ShawnMeister@users.noreply.github.com>
2020-07-06 03:16:11 -0700

    Add files via upload

---
+++ precache-manifest.6ff952b46f402e6e04f59b079203883d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9a5e4fd060fc1c6b3543f0348c549f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c1acbc0708e5edeb3e1",
+    "url": "/static/css/main.b0df65d0.chunk.css"
+  },
+  {
+    "revision": "12bdc19f29d439989ab2",
+    "url": "/static/js/2.0ba91e7e.chunk.js"
+  },
+  {
+    "revision": "d487d3db788af6bff7cfa405291a0c6c",
+    "url": "/static/js/2.0ba91e7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c1acbc0708e5edeb3e1",
+    "url": "/static/js/main.d6b069a2.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "263b274b6a7bcdeb86a1b8a27257fd0f",
+    "url": "/static/media/ShawnSmile.263b274b.png"
+  }
+]);
