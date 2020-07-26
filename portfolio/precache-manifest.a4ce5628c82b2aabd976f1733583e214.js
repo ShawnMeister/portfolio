d42941cd414a638397b05c25f1c45e5e2718e087
ShawnMeister <shawn.mountenay@gmail.com>
2020-07-26 11:35:57 -0700

    site update home page routing and bottom foreground

---
+++ portfolio/precache-manifest.a4ce5628c82b2aabd976f1733583e214.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc43fffaa37bd79ce71ff805e87526e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "838b4d1e7fe38722b6fe",
+    "url": "/static/css/main.b0df65d0.chunk.css"
+  },
+  {
+    "revision": "a20eef376d60a4948fb6",
+    "url": "/static/js/2.067a879a.chunk.js"
+  },
+  {
+    "revision": "7e7bbff4e5462d41d81ae3e6013b9896",
+    "url": "/static/js/2.067a879a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "838b4d1e7fe38722b6fe",
+    "url": "/static/js/main.1a9cd96a.chunk.js"
+  },
+  {
+    "revision": "b8836525d6198417091e",
+    "url": "/static/js/runtime-main.19f73f4b.js"
+  },
+  {
+    "revision": "cf68dfff9101ea3f6a60e8370464c765",
+    "url": "/static/media/mountains-bottom-foreground.cf68dfff.png"
+  }
+]);
