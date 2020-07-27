c2b7bf8315410bb7ea1d554d3267d414e00a098d
ShawnMeister <shawn.mountenay@gmail.com>
2020-07-26 23:03:20 -0700

    finished porfolio, still needs styles

---
+++ portfolio/react-3dAxe-landing/build/precache-manifest.c184a107b583a1036fe29513b72e83bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d76ed57865055d085dcafe19069b4d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a847de36c79856e9a46b",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "62dde8ba0fb74f081780",
+    "url": "/static/css/main.c989d0f6.chunk.css"
+  },
+  {
+    "revision": "a847de36c79856e9a46b",
+    "url": "/static/js/2.393e894b.chunk.js"
+  },
+  {
+    "revision": "59a02ae999aec25c5ab4324c3217be95",
+    "url": "/static/js/2.393e894b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62dde8ba0fb74f081780",
+    "url": "/static/js/main.f2420644.chunk.js"
+  },
+  {
+    "revision": "b8836525d6198417091e",
+    "url": "/static/js/runtime-main.19f73f4b.js"
+  },
+  {
+    "revision": "cf68dfff9101ea3f6a60e8370464c765",
+    "url": "/static/media/mountains-bottom-foreground.cf68dfff.png"
+  }
+]);
