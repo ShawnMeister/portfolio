9c2c0604fd48a26941f4e3d81d3e1f2119ef4087
ShawnMeister <shawn.mountenay@gmail.com>
2020-08-02 11:28:28 -0700

    hopefully routing works now

---
+++ precache-manifest.2c1a9be35155719f43fb948dfac83429.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7891fe49929e32bcc354c1ba6dd4d6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c37050f684d053658794",
+    "url": "/static/css/2.5f98dec3.chunk.css"
+  },
+  {
+    "revision": "506394c12f46db809430",
+    "url": "/static/css/main.e853c762.chunk.css"
+  },
+  {
+    "revision": "c37050f684d053658794",
+    "url": "/static/js/2.bb0bd8b4.chunk.js"
+  },
+  {
+    "revision": "59a02ae999aec25c5ab4324c3217be95",
+    "url": "/static/js/2.bb0bd8b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "506394c12f46db809430",
+    "url": "/static/js/main.19482bbb.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "cf68dfff9101ea3f6a60e8370464c765",
+    "url": "/static/media/mountains-bottom-foreground.cf68dfff.png"
+  }
+]);
