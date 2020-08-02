2dad8a4908c03e5db7f11d0b27ff04622131e95e
ShawnMeister <shawn.mountenay@gmail.com>
2020-08-02 12:52:23 -0700

    new build with deleted nav bar gradient

---
+++ precache-manifest.f2fd29f95f3cb98dc2da10705d9a1d1e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e95ef77ab4c847016f38a71977316ae8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e26b289dfd55e90dc9e5",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0c503b90639fcb119dec",
+    "url": "/static/css/main.a655499d.chunk.css"
+  },
+  {
+    "revision": "e26b289dfd55e90dc9e5",
+    "url": "/static/js/2.07b8c1df.chunk.js"
+  },
+  {
+    "revision": "59a02ae999aec25c5ab4324c3217be95",
+    "url": "/static/js/2.07b8c1df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c503b90639fcb119dec",
+    "url": "/static/js/main.b0140222.chunk.js"
+  },
+  {
+    "revision": "b8836525d6198417091e",
+    "url": "/static/js/runtime-main.19f73f4b.js"
+  },
+  {
+    "revision": "cf68dfff9101ea3f6a60e8370464c765",
+    "url": "/static/media/mountains-bottom-foreground.cf68dfff.png"
+  }
+]);
