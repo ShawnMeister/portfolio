fa7213fd091df82693c5f5dfa98b20375e3ac9b3
ShawnMeister <shawn.mountenay@gmail.com>
2020-08-04 19:37:16 -0700

    new build with animation update and favicon updated

---
+++ precache-manifest.b62e2e6c66d9b9d5a799ced73e27aca8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf586c42fd0458effa47ffbe546f341",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c37050f684d053658794",
+    "url": "/static/css/2.5f98dec3.chunk.css"
+  },
+  {
+    "revision": "abfc5274a2bc014058e1",
+    "url": "/static/css/main.aaaccdd3.chunk.css"
+  },
+  {
+    "revision": "c37050f684d053658794",
+    "url": "/static/js/2.bb0bd8b4.chunk.js"
+  },
+  {
+    "revision": "59a02ae999aec25c5ab4324c3217be95",
+    "url": "/static/js/2.bb0bd8b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abfc5274a2bc014058e1",
+    "url": "/static/js/main.e32e4ac0.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "cf68dfff9101ea3f6a60e8370464c765",
+    "url": "/static/media/mountains-bottom-foreground.cf68dfff.png"
+  }
+]);
