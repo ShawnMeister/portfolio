5076ee758c91d4ccc2e6927a2a3f4ea19a842b0a
ShawnMeister <shawn.mountenay@gmail.com>
2020-08-09 02:00:47 -0700

    updated bottom foreground filters

--- src/Components/BottomForeground/index.js
+++ src/Components/BottomForeground/index.js
@@ -18,7 +18,7 @@ export default (props) => {
 
         .imgOnly {
           width: 100vw;
-          filter: hue-rotate(200deg) grayscale(50%);
+          filter: hue-rotate(260deg) grayscale(66%);
         }
 
         // .imgOnly:hover {
